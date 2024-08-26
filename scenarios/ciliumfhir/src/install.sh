90b65fa1b3913be502ab1dc49334eae66a21d49c
Ron Sweeney <ron.sweeney@integrationrequired.com>
2024-08-26 10:09:45 -0400

    fix

--- scenarios/ciliumfhir/src/install.sh
+++ scenarios/ciliumfhir/src/install.sh
@@ -1,6 +1,6 @@
 irissession IRIS <<EOF
 zn "%SYS"
-Write $SYSTEM.OBJ.Load("/iris-scm/scenarios/ciliumfhir/cls/Coruscant.InterSystems.FHIR.cls","ck")
+Write \$SYSTEM.OBJ.Load("/iris-scm/scenarios/ciliumfhir/cls/Coruscant.InterSystems.FHIR.cls","ck")
 //zn "HSLIB"
 Set tSC = ##class(Coruscant.InterSystems.FHIR).Setup()
 h
